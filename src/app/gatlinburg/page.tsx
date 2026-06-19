716c363c9d6c3f908e8fba9f6d95fd5beee785b1
masondtorres <masondanieltorres@gmail.com>
2026-06-19 00:36:03 -0400

    Brand: use Smoky Insider public name

--- src/app/gatlinburg/page.tsx
+++ src/app/gatlinburg/page.tsx
@@ -133,7 +133,7 @@ export default function GatlinburgPage() {
     <main className="destination-page">
       <header className="destination-header">
         <Link className="wordmark" href="/">
-          Smokies Insider Guide
+          Smoky Insider
         </Link>
         <Link className="back-link" href="/start-planning">
           Start Planning
