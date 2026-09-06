0059134c72c6538b9526313b391ebef6a090a6c2
masondtorres <masondanieltorres@gmail.com>
2026-09-06 13:16:11 -0400

    Restore Smoky Insider site

--- api/gone.js
+++
@@ -1,6 +0,0 @@
-module.exports = function handler(req, res) {
-  res.statusCode = 410;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-store, max-age=0');
-  res.end('This website has been removed by its owner.');
-};
